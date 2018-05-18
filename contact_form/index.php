2019aec090f3018be2533692bb5e1e24fbc8c39a
fukuzono <fukuzono@genius-web.co.jp>
2018-05-18 15:32:17 +0900

    #9 ブランチの切り替え

---
+++ contact_form/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>お問い合わせフォーム | そして空になった</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=2.0">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link href="./../assets/css/common.css" rel="stylesheet" media="all">
+    <link href="./../assets/css/contact_form.css" rel="stylesheet" media="all">
+    <!--[if IE 9]>
+    <script src="/js/html5shiv.js"></script>
+    <script src="/js/css3-mediaqueries.js"></script>
+    <script src="/js/selectivizr-min.js"></script>
+    <![endif]-->
+    <!--[if lte IE 8]>
+    <script src="/js/html5shiv.js"></script>
+    <script src="/js/selectivizr-min.js"></script>
+    <![endif]-->
+</head>
+<body >
+    <header>
+        <div class="row">
+            <img src="./../assets/img/logo.png" width="250">
+        </div>
+
+    </header>
+
+        <section class="container">
+            <div class="row otoiawse text-center">
+                <div class="col-xs-12 text-center"><a class="btn btn-success" href="">お問い合わせはこちら</a></div>
+            </div>
+
+                <div class="row login-container">
+                    <div class="box login">
+                        <div class="login">
+                            <h3>お問い合わせ</h3>
+
+                            <form>
+                                <div class="form-group">
+                                    <label for="InputName">お名前</label>
+                                    <input type="user_name" class="form-control" id="InputName" placeholder="名前を入力して下さい。">
+                                </div>
+                                <div class="form-group">
+                                    <label for="InputEmail">メールアドレス</label>
+                                    <input type="email" class="form-control" id="InputEmail" placeholder="メールアドレスを入力して下さい。">
+                                </div>
+                                <div class="from-group">
+                                    <label for="InputTextarea">内容</label>
+                                    <textarea class="form-control" name="content" id="InputTextarea"></textarea>
+                                </div>
+                                <div class="text-center">
+                                    <input type="submit" value="確認" class="btn btn-success">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row text-center toroku-link">
+                    <a href="">会員登録はこちら（無料）</a>
+                </div>
+
+                <div class="this-box-all">
+                    <div class="row ttl">
+                        <h4>このサイトについて</h4>
+                    </div>
+                    <div class="row this-contents">
+                        <div class="col-xs-4 this-content">
+                            <div class="this-content-txt this-box">
+                                <p>冷蔵庫の中を管理できるよ！</p>
+                            </div>
+                        </div>
+                        <div class="col-xs-4 this-content">
+                            <div class="this-content-txt this-box">
+                                <p>栄養バランスを管理できるよ！</p>
+                            </div>
+                        </div>
+                        <div class="col-xs-4 this-content">
+                            <div class="this-content-txt this-box">
+                                <p>レシピ検索できるよ！</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+        </section>
+
+
+    <footer class="text-center">
+        <p>©︎Pistachio, Inc.</p>
+    </footer>
+
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
