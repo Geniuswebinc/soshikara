abc269ed2ef8081db36998d3ec89415c1c921379
fukuzono <fukuzono@genius-web.co.jp>
2018-05-28 09:46:41 +0900

    #5 meta変更

--- character/index.php
+++ character/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>栄養バランス | そして空になった</title>
+    <title>キャタクター | そして空になった</title>
     <meta name="keywords" content="">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=2.0">
